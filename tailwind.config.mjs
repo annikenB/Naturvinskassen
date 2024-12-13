5127afa51ee163a4d45f4ad6b6c669fb9b4de908
louisesg1 <loge001@stud.kea.dk>
2024-12-13 10:35:13 +0100

    fiksa

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,95 +15,93 @@ export default {
         500: "#hexkode",
         600: "#hexkode",
       },
-      
-
-      //     'white': '#fff',
-      //     'black': '#000',
-      // },
-
-      /* Tilføj din egen spacing nedenfor */
-      // spacing: {
-      //     'm': '40px',
-      // },
-
-      /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
-      // fontFamily: {
-      //     sans: ['Prompt', 'sans-serif'],
-      //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
-      // },
-
-      /* Tilføj dit typografiske hierarki herunder */
-      // fontSize: {
-      //     base: ['1rem', { lineHeight: '1.5rem' }],
-      //     large: ['2.5rem', { lineHeight: '3.75rem' }],
-      //   },
-
-      /* Tilføj dit fontvægt-hierarki nedenfor */
-      //   fontWeight: {
-      //     regular: '400',
-      //   },
-
-      fontFamily: {
-        sans: ["Manrope", "sans-serif"],
-        display: ["Londrina solid", "monospace"] /* eksempel på ekstra font */,
-      },
-
-      fontSize: {
-        base: ["1rem", { lineHeight: "1.5rem" }],
-        large: ["2.5rem", { lineHeight: "3.75rem" }],
-        maintext: ["1rem", { lineHeight: "1.5rem" }],
-        herotext: ["5rem", { lineHeight: "5.rem" }],
-        h1text: ["3.5rem", { lineHeight: "4rem" }],
-        h2text: ["3rem", { lineHeight: "4rem" }],
-        h3text: ["2rem", { lineHeight: "2.5rem" }],
-        h4text: ["1.5rem", { lineHeight: "2.5rem" }],
-      },
-
-      fontWeight: {
-        bold: "800",
-        semibold: "600",
-        regular: "400",
-        light: "100",
-      },
-
-      borderWidth: {
-        web: "2px",
-        mobile: "1px",
-      },
-
-      /* Borders - borderstørrelser herunder */
-      // borderWidth: {
-      //     DEFAULT: '1px',
-      //     0: '0px',
-      //     2: '2px',
-      // },
-
-      /* Border radius størrelser herunder */
-      // borderRadius: {
-      //     DEFAULT: '1.25rem',
-      //     none: '0px',
-      // },
-
-      spacing: {
-        m: "105px",
-      },
-      /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
-      // fontFamily: {
-      //     sans: ['Prompt', 'sans-serif'],
-      //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
-      // },
-
-      /* Tilføj dit fontvægt-hierarki nedenfor */
-      //   fontWeight: {
-      //     regular: '400',
-      //   },
-
-      /* Border radius størrelser herunder */
-      // borderRadius: {
-      //     DEFAULT: '1.25rem',
-      //     none: '0px',
-      // },
     },
+
+    //     'white': '#fff',
+    //     'black': '#000',
+    // },
+
+    /* Tilføj din egen spacing nedenfor */
+    // spacing: {
+    //     'm': '40px',
+    // },
+
+    /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
+    // fontFamily: {
+    //     sans: ['Prompt', 'sans-serif'],
+    //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
+    // },
+
+    /* Tilføj dit typografiske hierarki herunder */
+    // fontSize: {
+    //     base: ['1rem', { lineHeight: '1.5rem' }],
+    //     large: ['2.5rem', { lineHeight: '3.75rem' }],
+    //   },
+
+    /* Tilføj dit fontvægt-hierarki nedenfor */
+    //   fontWeight: {
+    //     regular: '400',
+    //   },
+
+    fontFamily: {
+      sans: ["Manrope", "sans-serif"],
+      display: ["Londrina solid", "monospace"] /* eksempel på ekstra font */,
+    },
+
+    fontSize: {
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      large: ["2.5rem", { lineHeight: "3.75rem" }],
+      maintext: ["1rem", { lineHeight: "1.5rem" }],
+      herotext: ["5rem", { lineHeight: "5.rem" }],
+      h1text: ["3.5rem", { lineHeight: "4rem" }],
+      h2text: ["3rem", { lineHeight: "4rem" }],
+      h3text: ["2rem", { lineHeight: "2.5rem" }],
+      h4text: ["1.5rem", { lineHeight: "2.5rem" }],
+    },
+
+    fontWeight: {
+      bold: "800",
+      semibold: "600",
+      regular: "400",
+      light: "100",
+    },
+
+    borderWidth: {
+      web: "2px",
+      mobile: "1px",
+    },
+
+    /* Borders - borderstørrelser herunder */
+    // borderWidth: {
+    //     DEFAULT: '1px',
+    //     0: '0px',
+    //     2: '2px',
+    // },
+
+    /* Border radius størrelser herunder */
+    // borderRadius: {
+    //     DEFAULT: '1.25rem',
+    //     none: '0px',
+    // },
+
+    /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
+    // fontFamily: {
+    //     sans: ['Prompt', 'sans-serif'],
+    //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
+    // },
+
+    /* Tilføj dit fontvægt-hierarki nedenfor */
+    //   fontWeight: {
+    //     regular: '400',
+    //   },
+
+    /* Border radius størrelser herunder */
+    // borderRadius: {
+    //     DEFAULT: '1.25rem',
+    //     none: '0px',
+    // },
+
+    extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
